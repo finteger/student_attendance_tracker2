b3e04f5b25112bcfd8db54675403d189619478e7
finteger <nashassociatesinc@gmail.com>
2024-12-03 19:14:46 -0700

    Update

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,13 +1,25 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const AttendanceManager = require('../models/attendanceManager.js');
 
 
 exports.login = async (req, res) =>{
     const { email, password } = req.body;
+    const secret = process.env.secret_key;
 
     try {
-        
+       const user = await AttendanceManager.findOne({email});
+
+       if(!user) {
+          return res.status(404).json('Invalid username.');
+       }
+       //Use bcrypt to verify the password
+       const result = await bcrypt.compare(password, user.password);
+  
+       //Generate the JWT
+       const token = jwt.sign({ id: user._id.toString()}, secret, { expiresIn:'5m'});
+
     } catch (error) {
         
     }
